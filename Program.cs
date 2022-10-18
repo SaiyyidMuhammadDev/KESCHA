12c066b0ffc53c7d9ed92e08a32fbb681f9659df
SayyidMuhammadDev <saidburkhanmukhtorov@gmail.com>
2022-10-18 11:39:57 +0500

    INFRA: Upload Project

--- Program.cs
+++ Program.cs
@@ -2,3 +2,15 @@
 System.Console.WriteLine($"Hello\nWhat is you name?:");
 string name = System.Console.ReadLine();
 System.Console.WriteLine($"Helle {name}. My name is Kescha. WELCOME");
+System.Console.Write("How old are you?:");
+string age = System.Console.ReadLine();
+int ageInt = Convert.ToInt32(age);
+if(ageInt>3){
+    System.Console.WriteLine("You older then Kescha");
+}
+else if(ageInt<3){
+    System.Console.WriteLine("You are younger then Kescha");
+}
+else {
+    System.Console.WriteLine("Your age eaual to Kescha age");
+}
